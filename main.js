c3b0372cf1595aa8d61215d2fe709281fda4d33b
WIHVI <112943833+WIHVI@users.noreply.github.com>
2023-01-18 15:03:22 +0200

    product cards section added

---
+++ main.js
@@ -0,0 +1,59 @@
+const shop = document.getElementById("shop");
+
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
+
+const generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((item) => {
+      const { id, name, price, desc, img } = item;
+      return `
+            <div id="product-id-${id}" class="item">
+                <img width="219" src=${img} alt="">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-quantity">
+                        <h2>$ ${price}</h2>
+                        <div class="buttons">
+                            <i class="bi bi-dash-lg"></i>
+                            <div id="${id}" class="quantity">0</div>
+                            <i class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    })
+    .join(""));
+};
+
+generateShop();
