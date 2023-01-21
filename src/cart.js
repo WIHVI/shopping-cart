16f4b0c57b10d28806000b8166c10c671c8f9bbc
WIHVI <112943833+WIHVI@users.noreply.github.com>
2023-01-21 15:16:03 +0200

    generateCartItems added to the cart.js

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,6 @@
+const label = document.getElementById("label");
+const shoppingCart = document.getElementById("shopping-cart");
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const cartAmountUpdate = () => {
@@ -9,3 +12,82 @@ const cartAmountUpdate = () => {
 };
 
 cartAmountUpdate();
+
+const generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((obj) => {
+        let { id, item } = obj;
+        let search = shopItemsData.find((obj) => obj.id === id) || [];
+        return `
+        <div class="cart-item">
+           <img width="100" src="${search.img}" alt="${search.name}">
+           <div class="details">
+              <div class="title-price-x">
+                <h4 class="title-price">
+                  <p>${search.name}</p>
+                  <p class="cart-item-price">$ ${search.price}</p>
+                </h4>
+                <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+              </div>
+              <div class="buttons">
+                  <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                  <div id="${id}" class="quantity">${item}</div>
+                  <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+              </div>
+              <h3>$ ${item * search.price}</h3>
+           </div>
+        </div>
+      `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+      <h2>Cart is Empty</h2>
+      <a href="index.html">
+        <button class="home-btn">Back to Home</button>
+      </a>
+    `;
+  }
+};
+
+generateCartItems();
+
+const increment = (id) => {
+  const selectedItem = id;
+  let search = basket.find((obj) => obj.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({ id: selectedItem.id, item: 1 });
+  } else {
+    search.item++;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+const decrement = (id) => {
+  const selectedItem = id;
+  let search = basket.find((obj) => obj.id === selectedItem.id);
+
+  if (search === undefined || search.item === 0) {
+    return;
+  } else {
+    search.item--;
+  }
+
+  update(selectedItem.id);
+  basket = basket.filter((obj) => obj.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+const update = (id) => {
+  let search = basket.find((obj) => obj.id === id);
+  document.getElementById(id).innerHTML = search.item;
+
+  cartAmountUpdate();
+};
